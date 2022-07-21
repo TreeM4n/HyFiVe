edeac984cfcc7ee39209801ec2d82162a885d73e
TreeM4n <baum.tree.tm@gmail.com>
2022-07-21 14:15:34 +0200

    tootlitp2

--- js/chartmultilegend.js
+++ js/chartmultilegend.js
@@ -14,7 +14,9 @@ const margin = { top: 30, right: 0, bottom: 30, left: 50 },
 var formatTime = d3.timeFormat("%Y-%m-%d %H:%M:%S");
 
 var sumstat;
-
+function dummy (number) {
+  return number +3;
+}
 export function create() {
 
   var data = sessionStorage.getItem("response");
@@ -36,10 +38,10 @@ export function create() {
     d.MS5837Press = +d.MS5837Press;
     d.Pressure = +d.MS5837Press;
     d.Conducitvity = +d.Conducitvity;
-    d.Salinity = +d.Conducitvity + 3;
+    d.Salinity = dummy(+d.Conducitvity);
 
 
-    //d.Foo = +d.TSYTemperatrue;
+    d.Foo = +d.TSYTemperatrue;
 
 
     for (var prop in d) {
@@ -66,11 +68,11 @@ export function create() {
 
       }
       //threshhold function
-      if (config.thresholdProp.indexOf(prop) != -1 && config.thresholdValues[config.thresholdProp.indexOf(prop)][0] != "")  {
-         
+      if (config.thresholdProp.indexOf(prop) != -1 && config.thresholdValues[config.thresholdProp.indexOf(prop)][0] != "") {
+
         if (config.thresholdValues[config.thresholdProp.indexOf(prop)][0] > value
           || config.thresholdValues[config.thresholdProp.indexOf(prop)][1] < value) {
-            var timeStorage = formatTime(d.time);
+          var timeStorage = formatTime(d.time);
 
           d3.select("#ULerror")
             .selectAll('myOptions')
@@ -134,7 +136,7 @@ function createsmallmultiple(data) {
   var diff = 0;
   //console.log(data)
   // group the data: I want to draw one line per group
-   sumstat = d3.group(data, d => d.y) // nest function allows to group the calculation per level of a factor
+  sumstat = d3.group(data, d => d.y) // nest function allows to group the calculation per level of a factor
   //console.log(sumstat)
   var svg;
   //d3.select('svg').remove();
@@ -142,7 +144,7 @@ function createsmallmultiple(data) {
 
   // List of groups (here I have one group per column)
   var allGroup = new Set(data.map(d => d.depl))
- 
+
 
 
   // add the options to the list
@@ -226,24 +228,24 @@ function createsmallmultiple(data) {
   const line = svg.append('g')
     .attr("clip-path", "url(#clip)")
   // color palette
-/*
-        // Set the gradient
-        svg.append("linearGradient")
-        .attr("id", "line-gradient")
-        .attr("gradientUnits", "userSpaceOnUse")
-        .attr("x1", 0)
-        .attr("y1", y(0))
-        .attr("x2", 0)
-        .attr("y2", y(max))
-        .selectAll("stop")
-          .data([
-            {offset: "0%", color: "blue"},
-            {offset: "100%", color: "red"}
-          ])
-        .enter().append("stop")
-          .attr("offset", function(d) { return d.offset; })
-          .attr("stop-color", function(d) { return d.color; });
-          */
+  /*
+          // Set the gradient
+          svg.append("linearGradient")
+          .attr("id", "line-gradient")
+          .attr("gradientUnits", "userSpaceOnUse")
+          .attr("x1", 0)
+          .attr("y1", y(0))
+          .attr("x2", 0)
+          .attr("y2", y(max))
+          .selectAll("stop")
+            .data([
+              {offset: "0%", color: "blue"},
+              {offset: "100%", color: "red"}
+            ])
+          .enter().append("stop")
+            .attr("offset", function(d) { return d.offset; })
+            .attr("stop-color", function(d) { return d.color; });
+            */
   // Draw the line
   line.append("path")
     .attr("class", "line")
@@ -257,7 +259,7 @@ function createsmallmultiple(data) {
       var max = d3.max(d[1], function (d) { return +d.value; })
 
       var mapY = d3.scaleLinear()
-         .domain([min * 5 / 6, max * 7 / 6])
+        .domain([min * 5 / 6, max * 7 / 6])
         .range([height, 0])
 
       var lineGen = d3.line()
@@ -282,7 +284,7 @@ function createsmallmultiple(data) {
 
     })
 
- 
+
 
 
   //-----------------------------------
@@ -304,35 +306,125 @@ function createsmallmultiple(data) {
     .attr("y", -5)
     .attr("x", 0)
     .text(function (d) {
-        if (config.thresholdUnits[config.thresholdProp.indexOf(d[0])] != undefined) {
-            return (d[0]+"("+ config.thresholdUnits[config.thresholdProp.indexOf(d[0])]) +")" 
-        }
-    else {
+      if (config.thresholdUnits[config.thresholdProp.indexOf(d[0])] != undefined) {
+        return (d[0] + "(" + config.thresholdUnits[config.thresholdProp.indexOf(d[0])]) + ")"
+      }
+      else {
         return d[0]
-    }
+      }
     })
     .style("fill", function (d) { return config.chartcolor(d[0]) })
 
 
+  //-----------------------------------------------------------
+   // This allows to find the closest X index of the mouse:
+   var bisect = d3.bisector(function(d) { return d.x; }).left;
 
-   svg.on("click", function (event, d) {
-        var time = x.invert(d3.pointer(event)[0]);
-  
-     var formatTime = d3.timeFormat("%Y-%m-%d %H:%M");
-    var dataFilter = data.filter(function (d) { return formatTime(d.x) == formatTime(time)})
-    //console.log(dataFilter[0])
-    document.getElementById('stats').textContent = dataFilter[0].y + dataFilter[0].depl;
+   // Create the circle that travels along the curve of chart
+   var focus = svg
+     .append('g')
+     .append('circle')
+       .style("fill", "none")
+       .attr("stroke", "black")
+       .attr('r', 8.5)
+       .style("opacity", 0)
+ 
+   // Create the text that travels along the curve of chart
+   var focusText = svg
+     .append('g')
+     .append('text')
+       .style("opacity", 0)
+       .attr("text-anchor", "left")
+       .attr("alignment-baseline", "middle")
+
+  // Create a rect on top of the svg area: this rectangle recovers mouse position
+  svg
+    .append('rect')
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .attr('width', width)
+    .attr('height', height)
+    .on('mouseover', mouseover)
+    .on('mousemove', mousemove)
+    .on('mouseout', mouseout);
+
+
+  // What happens when the mouse move -> show the annotations at the right positions.
+  function mouseover() {
+    focus.style("opacity", 1)
+    focusText.style("opacity",1)
+  }
+
+  function mousemove(event, d) {
+    // recover coordinate we need
+    var x0 = x.invert(d3.pointer(event)[0]);
+    var i = bisect(data, x0, 1);
+    //console.log(data[i])
+    var formatTime = d3.timeFormat("%Y-%m-%d %H:%M:%S");
+    var dataFilter = data.filter(function (d) { return formatTime(d.x) == formatTime(data[i].x) })
+    //console.log(dataFilter)
+    
+    focus
+      .attr("cx", d3.pointer(event)[0])
+      .attr("cy", d3.pointer(event)[1])
+    focusText
+      .html("y:"+ dataFilter[0].value  )
+      .attr("x", d3.pointer(event)[0])
+      .attr("y", d3.pointer(event)[1])
+      
+    }
+    
+  function mouseout() {
+    focus.style("opacity", 0)
+    focusText.style("opacity", 0)
+  }
+
+
+/*
+  svg.on("mousemove", function (event, d) {
+    var time = x.invert(d3.pointer(event)[0]);
+    //console.log((d3.pointer(event, this)));
+    d3.select("stats").remove();
+
+
+    var formatTime = d3.timeFormat("%Y-%m-%d %H:%M:%S");
+    var dataFilter = data.filter(function (d) { return formatTime(d.x) == formatTime(time) })
+    //console.log(dataFilter)
+    if (dataFilter.length != 0 && dataFilter) {
+      for (var i in dataFilter) {
+        // console.log(i)
+        // document.getElementById('stats').textContent += dataFilter[i].y + dataFilter[i].y + "\n";
+
+
+      }
+
+      tooltip = d3.select("#stats")
+        .selectAll('myOptions')
+        .data([dataFilter])//to generate just one
+        .enter()
+        .append('li')
+        //get start date 
+        .text(function (d) {
+          return x.invert(d3.pointer(event, this)[0])
+          //d[0].y + " was:" + d[0].value 
+        })
+        .style('padding', '4px')
+        .style('color', 'orange')
+        .attr("value", -1) // corresponding value returned by the button
+
       mapJS.showpoint(x.invert(d3.pointer(event)[0]))
-     
+    }
 
   });
+*/
+  //----------------------------------------------------------------------------------------
 
   // A function that update the chart for given boundaries after brushing
   function updateChart(event, d) {
     var dataFilter = data;
     // What are the selected boundaries?
     var extent = event.selection;
-   // console.log(x.invert(extent[0]))
+    // console.log(x.invert(extent[0]))
     //console.log(extent);
     // If no selection, back to initial coordinate. Otherwise, update X axis domain
     if (!extent) {
@@ -344,9 +436,9 @@ function createsmallmultiple(data) {
     } else {
 
       x.domain([x.invert(extent[0]), x.invert(extent[1])])
-      
+
       dataFilter = data.filter(function (d) { return d.x >= x.invert(extent[0]) })
-      dataFilter = dataFilter.filter(function (d) { return d.x<= x.invert(extent[1]) })
+      dataFilter = dataFilter.filter(function (d) { return d.x <= x.invert(extent[1]) })
       //datafilter here is broken, shortens the start and end time
       line.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
 
@@ -393,7 +485,7 @@ function createsmallmultiple(data) {
         var max = d3.max(dataFilter2, function (d) { return +d.value; })
 
         var mapY = d3.scaleLinear()
-           .domain([min * 5 / 6, max * 7 / 6])
+          .domain([min * 5 / 6, max * 7 / 6])
           .range([height, 0])
 
         var lineGen = d3.line()
@@ -477,7 +569,7 @@ function createsmallmultiple(data) {
 
 
   svg.on("mouseover", function (d) {
-  
+
     d3.select(this)
       .style("stroke", "black")
       .style("opacity", 1)
@@ -492,9 +584,9 @@ function createsmallmultiple(data) {
   })
 
 
-   //---------------mooseover
+  //---------------mooseover
 
- 
+
 
 
   // When the button is changed, run the updateChart function
@@ -504,7 +596,7 @@ function createsmallmultiple(data) {
 
     //console.log( event.explicitOriginalTarget)
     if (selectedOption == 0) {
-      
+
       x.domain(d3.extent(data, function (d) { ; return d.x; }))
       // Update axis and line position
       xAxis
@@ -575,12 +667,12 @@ function createsmallmultiple(data) {
 }
 
 
-export function resetCharts () {
-    d3.selectAll('svg').remove();
-    d3.selectAll('option').remove();
-     d3.selectAll('li').remove();
-    
-    create();
+export function resetCharts() {
+  d3.selectAll('svg').remove();
+  d3.selectAll('option').remove();
+  d3.selectAll('li').remove();
+
+  create();
 }
 
 
